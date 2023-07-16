4c1faeb167aa5fdc4f772cbc8aa230a891410cbc
Tarek Rahal <tarekrahale@gmail.com>
2023-07-16 21:42:33 +0100

    fixed capitalization in floating benefits paragraphs

--- src/components/floating-benefits/floating-benefits.client.tsx
+++ src/components/floating-benefits/floating-benefits.client.tsx
@@ -9,7 +9,7 @@ export default function FloatingBenefits() {
         {
             title: 'Reduces Anxiety & Stress',
             imagePath: '/images/mandala@2x.png',
-            content: 'imparts more magnesium in your system and lowers levels of cortisol.in the absence of stimulants you achieve the same relaxation level of a 4 hours REM sleep within an hour of floating.'
+            content: 'Imparts more magnesium in your system and lowers levels of cortisol.in the absence of stimulants you achieve the same relaxation level of a 4 hours REM sleep within an hour of floating.'
         },
         {
             title: 'Promotes mindfulness & deep meditation',
@@ -19,7 +19,7 @@ export default function FloatingBenefits() {
         {
             title: 'Replenishes creativity and focus',
             imagePath: '/images/dumbell@2x.png',
-            content: 'explore a dreamy state, The Theta State and deep relaxation of floating allows you to tap into the creative and intellectual power of your subconscious mind.'
+            content: 'Explore a dreamy state, The Theta State and deep relaxation of floating allows you to tap into the creative and intellectual power of your subconscious mind.'
         }
     ]
 
