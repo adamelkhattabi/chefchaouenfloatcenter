4b6e5dc30cf343cef4e558287b6c72450f4a6244
Tarek Rahal <tarekrahale@gmail.com>
2023-08-05 17:45:01 +0100

    changed footer links

--- src/components/footer/footer.client.tsx
+++ src/components/footer/footer.client.tsx
@@ -11,37 +11,27 @@ export default function Footer() {
                         <Link to='/' className={styles.footer_brand}>
                             <img width={'100%'} src="/images/logo___1_-removebg-preview.png" />
                         </Link>
-                        <a className={styles.footer_info} href='mailto:info@chefchaouenfloating.com'>
+                        <a className={styles.footer_info} href='mailto:contact@chefchaouenfloating.com'>
                             <span className={styles.footer_mailIcon}>
                                 <img src='/images/email@2x.png' />
                             </span>
-                            <span className={styles.footer_mail}>&nbsp;info@chefchaouenfloating.com&nbsp;</span>
+                            <span className={styles.footer_mail}>&nbsp;contact@chefchaouenfloating.com&nbsp;</span>
                         </a>
-                        <a className={styles.footer_info} href='tel:0666666666'>
+                        <a className={styles.footer_info} href='tel:+212622156902'>
                             <span className={styles.footer_mailIcon}>
                                 <img src='/images/phone@2x.png' />
                             </span>
-                            <span className={styles.footer_mail}>&nbsp;0666666666&nbsp;</span>
+                            <span className={styles.footer_mail}>&nbsp;+212622156902&nbsp;</span>
                         </a>
                         <ul className={styles.footer_social}>
                             <li>
-                                <a href='https://web.facebook.com/FALOOUJA' target='_blank'>
+                                <a href='#'>
                                     <img src='/images/f-b@2x.png' />
                                 </a>
                             </li>
                             <li>
-                                <a href='https://www.instagram.com/moul_ndader_officiel/' target='_blank'>
+                                <a href='https://www.instagram.com/chefchaouen_floating_center/' target='_blank'>
                                     <img src='/images/insta@2x.png' />
-                                </a>
-                            </li>
-                            <li>
-                                <a href='https://twitter.com/moul_ndader2' target='_blank'>
-                                    <img src='/images/twitter@2x.png' />
-                                </a>
-                            </li>
-                            <li>
-                                <a href='https://www.linkedin.com/in/moul-ndader-80493819b/' target='_blank'>
-                                    <img src='/images/linked@2x.png' />
                                 </a>
                             </li>
                         </ul>
