84f52c7901a1934148d3a15bb5efab8b28f1fe23
Tarek Rahal <tarekrahale@gmail.com>
2023-08-14 00:12:51 +0100

    added new fonts

--- src/components/page-title/page-title.client.tsx
+++ src/components/page-title/page-title.client.tsx
@@ -8,8 +8,8 @@ interface TitleProps {
 export default function PageTitle({ title }: TitleProps) {
 
     return (<div className="container">
-        <Typography variant="h1" align="center" classes={{h1: styles.title}}>
+        <h1 className={styles.title}>
             { title }
-        </Typography>
+        </h1>
     </div>)
 }
